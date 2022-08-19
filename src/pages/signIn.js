3f42cde3e53f53b9802b3d8b208f69f217b16413
Jenet <xuanchen0910@gmail.com>
2022-08-19 17:00:17 +0900

    feat: 로그인페이지에 회원가입 페이지 연결하기(Link)

--- src/pages/signIn.js
+++ src/pages/signIn.js
@@ -1,6 +1,17 @@
 import { Button, Input } from "antd";
 import styled from "@emotion/styled";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { fetchPostLogin } from "../api/fetchData";
+import useForm from "../hooks/useForm";
 const SignIn = () => {
+  const navigate = useNavigate();
+
+  fetchPostLogin({
+    email: "jenet@email.com",
+    password: "12345678",
+  });
+
   return (
     <Wrapper>
       <h1>로그인</h1>
@@ -10,6 +21,9 @@ const SignIn = () => {
         <Button type="primary" shape="round" size="large">
           Sign In!
         </Button>
+        <GoSignUp>
+          오늘 처음이신가요? <Link to={"/signUp"}>회원가입 하기</Link>
+        </GoSignUp>
       </FormContainer>
     </Wrapper>
   );
@@ -28,4 +42,11 @@ const FormContainer = styled.form`
   display: flex;
   justify-content: space-around;
   flex-direction: column;
+`;
+
+const GoSignUp = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  gap: 10px;
 `;
