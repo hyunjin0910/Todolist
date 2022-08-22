efa8b3f5a664e176bba56e8ee4dbef314900bc2c
Jenet <xuanchen0910@gmail.com>
2022-08-22 19:49:22 +0900

    feat: 더미데이터로 추가하기 기능 구현

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,9 +1,34 @@
 import styled from "@emotion/styled";
 import { Button, Input } from "antd";
-const TodoItem = () => {
-  return <TodoWrapper></TodoWrapper>;
+import React from "react";
+import { Edit2, Trash2 } from "react-feather";
+import { useState } from "react";
+const TodoItem = ({ data }) => {
+  const [isEdit, setIsEdit] = useState(false);
+  const [isDeleted, setIsDeleted] = useState(false);
+  const handleEdit = () => {
+    setIsEdit(!isEdit);
+  };
+  const handleDelete = () => {
+    setIsDeleted(!isDeleted);
+  };
+  return (
+    <TodoWrapper>
+      <Input
+        type="checkbox"
+        style={{
+          width: 20,
+        }}
+      />
+      {isEdit ? <Input value={data.title} /> : <span>{data.title}</span>}
+      <Edit2 onClick={handleEdit} />
+      <Trash2 onClick={handleDelete} />
+    </TodoWrapper>
+  );
 };
 
 export default TodoItem;
 
-const TodoWrapper = styled.div``;
+const TodoWrapper = styled.div`
+  display: flex;
+`;
--- src/pages/todoList.js
+++ src/pages/todoList.js
@@ -1,9 +1,8 @@
 import { Button, Input } from "antd";
 import styled from "@emotion/styled";
 import { useState } from "react";
-import TodoItem from "../components/todoItem";
+import TodoItem from "../components/TodoItem";
 const TodoList = () => {
-  const [todos, setTodos] = useState([]);
   const dummy = [
     {
       id: 1,
@@ -21,12 +20,37 @@ const TodoList = () => {
       isDone: true,
     },
   ];
+  const [todos, setTodos] = useState(dummy);
+  const [newTodo, setNewTodo] = useState("");
+  const handleAddClick = () => {
+    setTodos([...todos, { id: todos.length + 1, title: newTodo, isdone: false }]);
+  };
   return (
-    <>
-      <Input placeholder="할일을 입력하세요" />
-      <Button>추가하기</Button>
-    </>
+    <ListWrapper>
+      <h1>나의 할일 목록</h1>
+      <Input
+        placeholder="할일을 입력하세요"
+        style={{
+          width: "calc(100% - 200px)",
+        }}
+        size="large"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+      />
+      <Button type="primary" size="large" onClick={handleAddClick}>
+        추가하기
+      </Button>
+      {todos.map((todo, idx) => (
+        <TodoItem data={todo} key={idx} />
+      ))}
+    </ListWrapper>
   );
 };
 
 export default TodoList;
+
+const ListWrapper = styled.div`
+  width: 100%;
+  margin-top: 100px;
+  text-align: center;
+`;
