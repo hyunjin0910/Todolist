4fc277a12c5d73a4b7a50f535b1c7fc157794f89
Jenet <xuanchen0910@gmail.com>
2022-08-22 20:39:07 +0900

    design: 체크박스 클릭시  text-decoration: line-through;

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,11 +1,15 @@
 import styled from "@emotion/styled";
-import { Button, Input } from "antd";
+import { Input } from "antd";
 import React from "react";
 import { Edit2, Trash2 } from "react-feather";
 import { useState } from "react";
 const TodoItem = ({ data }) => {
+  const [isChecked, setIsChecked] = useState(false);
   const [isEdit, setIsEdit] = useState(false);
   const [isDeleted, setIsDeleted] = useState(false);
+  const handleCheck = () => {
+    setIsChecked(!isChecked);
+  };
   const handleEdit = () => {
     setIsEdit(!isEdit);
   };
@@ -19,10 +23,26 @@ const TodoItem = ({ data }) => {
         style={{
           width: 20,
         }}
+        onClick={handleCheck}
       />
-      {isEdit ? <Input value={data.title} /> : <span>{data.title}</span>}
-      <Edit2 onClick={handleEdit} />
-      <Trash2 onClick={handleDelete} />
+      <InputContainer>
+        {isEdit ? (
+          <Input
+            value={data.title}
+            style={{
+              width: 100,
+            }}
+          />
+        ) : (
+          <span className={isChecked ? "checked" : ""}>{data.title}</span>
+        )}
+      </InputContainer>
+      <IconContainer>
+        <Edit2 onClick={handleEdit} />
+      </IconContainer>
+      <IconContainer>
+        <Trash2 onClick={handleDelete} />
+      </IconContainer>
     </TodoWrapper>
   );
 };
@@ -31,4 +51,19 @@ export default TodoItem;
 
 const TodoWrapper = styled.div`
   display: flex;
+  margin: 0.5rem 0;
+`;
+
+const InputContainer = styled.div`
+  width: 10rem;
+  padding: 0.2rem 0;
+
+  .checked {
+    text-decoration: line-through;
+  }
+`;
+
+const IconContainer = styled.div`
+  padding: 0.1rem 0.4rem;
+  cursor: pointer;
 `;
