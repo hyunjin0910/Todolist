d61d62cdd06713962d39fdeea21c823a49d75bce
Jenet <xuanchen0910@gmail.com>
2022-08-23 13:33:43 +0900

    design: 홈화면 꾸미기

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,30 @@
+import React from "react";
+import { FileText } from "react-feather";
+import { useNavigate } from "react-router-dom";
+import styled from "@emotion/styled";
+import { Button } from "antd";
 const Home = () => {
-  return <>홈</>;
+  const navigate = useNavigate();
+  return (
+    <Wrapper>
+      <FileText size="40px" />
+      <h1 style={{ color: "blue" }}> TO DO LIST</h1>
+      <UserWrapper>
+        <Button onClick={() => navigate("/signIn")}>로그인하기</Button>
+        <Button onClick={() => navigate("/signUp")}>회원 가입하기</Button>
+      </UserWrapper>
+    </Wrapper>
+  );
 };
 
 export default Home;
+
+const Wrapper = styled.div`
+  margin-top: 200px;
+  text-align: center;
+`;
+
+const UserWrapper = styled.div`
+  display: flex;
+  margin-top: 100px;
+`;
