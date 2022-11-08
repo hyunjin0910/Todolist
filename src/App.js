7157e44ebe8daddca0b38b2ec173467e91ad2815
Jenet <xuanchen0910@gmail.com>
2022-11-08 10:49:36 +0900

    remove: redux-toolkit 내용 삭제

--- src/App.js
+++ src/App.js
@@ -2,22 +2,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { SignIn, SignUp, Home, TodoList } from "./pages/index";
 import "./App.css";
 import "antd/dist/antd.css";
-import { store } from "./store";
-import { Provider } from "react-redux";
 
 const App = () => {
   return (
     <div className="app">
-      <Provider store={store}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/signUp" element={<SignUp />} />
-            <Route path="/signIn" element={<SignIn />} />
-            <Route path="/todos" element={<TodoList />}></Route>
-          </Routes>
-        </BrowserRouter>
-      </Provider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signUp" element={<SignUp />} />
+          <Route path="/signIn" element={<SignIn />} />
+          <Route path="/todos" element={<TodoList />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
